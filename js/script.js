c61670065334c993637c337c080670c0ace6b67b
Lamim <lemon07r@gmail.com>
2019-04-12 03:54:48 -0400

    Fixed sticky navbar with JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 /** Add any JavaScript you need to this file. */
+window.onscroll = function () {
+    myFunction()
+};
+
+let header = document.getElementById("navBar");
+let sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
