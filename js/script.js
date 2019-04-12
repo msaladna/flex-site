928350332a4540f053f7a28e75c9eb9cc715790d
Lamim <lemon07r@gmail.com>
2019-04-12 19:19:11 -0400

    page populators in development

--- js/script.js
+++ js/script.js
@@ -16,13 +16,37 @@ function navSticky() {
 
 // Menu Handler Functions
 (function () {
-    var currentPage = 'home';
-    var pageHelper = {
+    let currentPage = 'home';
+    const pageHelper = {
         // Clears the main element
         clearPage: function (page) {
             if (page !== currentPage)
                 document.getElementById('mainSection').innerHTML = "";
         },
+        mainPage: function (page) {
+            if (page === currentPage)
+                return;
+            document.getElementById('mainSection').innerHTML = `
+            <div class="row">
+                <div class="col-md d-none d-md-flex things2slide">
+                    <div class="mr-1">
+                        <img src="images/shake1.jpg" class="img img-fluid" alt="Protein Shake" id="proShake">
+                    </div>
+                </div>
+                <div class="col-12 col-md" id="description">
+                    <h2>Shop with confidence..</h2>
+                    <p>
+                        Here at Flex Fuels we strive to be the very best at what we do,
+                        because we know how important is to be putting in only the very best fuel in your body.
+                        You are what you eat after all!
+                        <br><br>Shop for the best supplements at the most competitive prices here
+                        at Flex Fuel. Our store carries everything from the tastiest whey powder mixes to the most convenient
+                        vitamin pills to help you get what your body needs to keep going at maximum proficiency.
+                        <br><br>Don't settle for just any fuel, get Flex Fuels.
+                    </p>
+                </div>
+            </div>`;
+        }
     };
 
     function setupMenuHandlers() {
@@ -30,11 +54,13 @@ function navSticky() {
             .getElementById('home1')
             .addEventListener('click', function () {
                 pageHelper.clearPage('home');
+                pageHelper.mainPage('home');
             });
         document
             .getElementById('home2')
             .addEventListener('click', function () {
                 pageHelper.clearPage('home');
+                pageHelper.mainPage('home');
             });
         document
             .getElementById('store1')
@@ -67,5 +93,6 @@ function navSticky() {
                 pageHelper.clearPage('about');
             });
     }
+
     window.onload = setupMenuHandlers;
 })();
